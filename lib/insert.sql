02e99cabeda2d62aaedfc81e3470f067fa6c135a
kevin-wairi <kevinwairi@gmail.com>
2023-05-27 22:02:10 +0300

    assignment completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',10,'M','White','sanguine',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy',20,'F','yellow','phlegmatic',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabitha',30,'F','pink','choleric',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown',40,'M','black','melancholic',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Melissa',10,'F','red','sanguine',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Grinch',50,'M','White','phlegmatic',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy',10,'F','black','melancholic',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,50,'M','White','sanguine',1);
